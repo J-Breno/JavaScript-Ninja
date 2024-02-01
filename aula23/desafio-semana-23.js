30f3763c03c2d09a5cab48a30509a08157c7c664
J-Breno <joao.breno85@hotmail.com>
2024-02-01 15:58:01 -0300

    refatorando a calculadora, irei depois colocar outras coisas nela

--- aula23/desafio-semana-23.js
+++ aula23/desafio-semana-23.js
@@ -1,5 +1,5 @@
 (function (win, doc) {
-  "use strict";
+  'use strict';
 
   /*
     Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
@@ -26,6 +26,8 @@
     input;
     - Ao pressionar o botão "CE", o input deve ficar zerado.
     */
+
+    
   const $input = doc.querySelector('[data-js="input-calculator"]');
   const $numbers = doc.querySelectorAll('[data-js="numbers"]');
   const $deleteDel = doc.querySelector('[data-js="delete-del"]');
@@ -33,16 +35,22 @@
   const $operators = doc.querySelectorAll('[data-js="operators"]');
   const $result = doc.querySelector('[data-js="result"]');
 
-  Array.prototype.forEach.call($numbers, function (button) {
-    button.addEventListener("click", handleClickNumber, false);
-  });
+    function initialize() {
+      iniEvents();
+    };
 
-  Array.prototype.forEach.call($operators, function (button) {
-    button.addEventListener("click", handleClickOperation, false);
-  });
+    function iniEvents() {
+      Array.prototype.forEach.call($numbers, function (button) {
+        button.addEventListener("click", handleClickNumber, false);
+      });
+      Array.prototype.forEach.call($operators, function (button) {
+        button.addEventListener("click", handleClickOperation, false);
+      });
+      $deleteAc.addEventListener('click', handleClickAc, false);
+      $result.addEventListener("click", handleClickResult, false);
+    }
 
-  $deleteAc.addEventListener('click', handleClickAc, false);
-  $result.addEventListener("click", handleClickResult, false);
+    
 
   function handleClickNumber() {
     $input.value += this.value;
@@ -58,41 +66,60 @@
   }
 
   function isLastItemAnOperation(number) {
-    var operations = ['+', '-', '*', '/', '%'];
+    var operations = getOperations();
     var lastItem = number.split('').pop();
     return operations.some(function(operator) {
       return operator === lastItem;
     });
   }
 
-  function removeLastItemIfItIsAnOperator(number) {
-    if(isLastItemAnOperation(number)) {
-      return number.slice(0, -1);
-    }
-    return number;
+  function getOperations() {
+      return Array.prototype.map.call($operators, function(button) {
+        return button.value;  
+      });
+  }
+
+  function removeLastItemIfItIsAnOperator(string) {
+    if(isLastItemAnOperation(string)) 
+      return string.slice(0, -1);
+    return string;
   }
 
   function handleClickResult() {
     $input.value = removeLastItemIfItIsAnOperator($input.value);
-    var allValues = $input.value.match(/\d+[+\*\/-]?/g);
-    $input.value = allValues.reduce(function(accumulated, actual) {
-      var firstValue = accumulated.slice(0, -1);
-      var operator = accumulated.split('').pop();
-      var lastValue = removeLastItemIfItIsAnOperator(actual);
-      var lastOperator = isLastItemAnOperation(actual) ? actual.split('').pop() : '';
-      switch(operator) {
-        case '+':
-          return ( Number(firstValue) + Number(lastValue) ) + lastOperator;
-        case '-':
-          return ( Number(firstValue) - Number(lastValue) ) + lastOperator;
-        case '*':
-          return ( Number(firstValue) * Number(lastValue) ) + lastOperator;
-        case '/':
-          return ( Number(firstValue) / Number(lastValue) ) + lastOperator;
-        case '%':
-          return ( Number(firstValue) % Number(lastValue) ) + lastOperator;
-      }
-    });
+    var allValues = $input.value.match(getRegexOperations());
+    $input.value = allValues.reduce(calculateAllValues);
+  }
+
+  function getRegexOperations() {
+    return new RegExp('\\d+[' + getOperations().join('')+']?', 'g');
+  }
+
+  function calculateAllValues(accumulated, actual) {
+    var firstValue = accumulated.slice(0, -1);
+    var operator = accumulated.split('').pop();
+    var lastValue = removeLastItemIfItIsAnOperator(actual);
+    var lastOperator = getLastOperator(actual);
+    return doOperation(operator,firstValue, lastValue) + lastOperator;
+  }
+
+  function getLastOperator(value) {
+   return isLastItemAnOperation(value) ? value.split('').pop() : '';
+  }
+
+  function doOperation(operator, firstValue, lastValue) {
+    switch(operator) {
+      case '+':
+        return  Number( firstValue ) + Number( lastValue ) ;
+      case '-':
+        return  Number( firstValue ) - Number( lastValue ) ;
+      case '*':
+        return  Number( firstValue ) * Number( lastValue ) ;
+      case '/':
+        return  Number( firstValue ) / Number( lastValue ) ;
+      case '%':
+        return  Number( firstValue ) % Number( lastValue ) ;
+    }
   }
 
   $deleteDel.addEventListener(
@@ -102,4 +129,18 @@
     },
     false
   );
+
+  initialize(); // sempre tenha uma funçao que vai iniciar a sua aplicação
+
+  // se algo não estiver muito claro, você deve estrair ela para uma função
+
+  // nunca quebre linha dentro de uma função, se você precisar quebrar linha dentro de uma função, refatore o seu código e faça outra function, pois com toda a certeza ele precisa ser reformulado;
+
+  // no máximo se for ter que utlizar if, utilize if de no max 4 linhas
+
+  // tente manter arquivos pequenos de no max 100 linhas e tente não usar comentários(eu estou utilizando porque são anotações de estudo), mas nos projetos não precisa.
+
+  
+  
+
 })(window, document);
